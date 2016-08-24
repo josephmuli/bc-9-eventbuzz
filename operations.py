7919db303a90bbff1e21a0d60cde6bf30dbfb33e
joseph muli <jayjaymuli00@gmail.com>
2016-08-24 03:52:31 +0300

    (Feature) new list method
    add list_events method that queries and returns all event entries in the database

--- operations.py
+++ operations.py
@@ -19,14 +19,19 @@ class EventsCrud(object):
 		event = Event(name, start_date, end_date, venue)
 
 		# add event object to session
-		session.add(event)
-		session.commit()
+		EventsCrud.session.add(event)
+		EventsCrud.session.commit()
 
 
 	def delete_event(self, event_id):
+		
 		# query item by id given
-		deleted_item = session.query(Event).filter(Event.id == event_id)
-		session.delete(deleted_item='evaluate')
-		session.commit()
+		deleted_item = EventsCrud.session.query(Event).filter(Event.id == event_id)
+		EventsCrud.session.delete(deleted_item='evaluate')
+		EventsCrud.session.commit()
 
 
+	def list_events(self):
+
+		events = EventsCrud.session.query.all()
+		return events
