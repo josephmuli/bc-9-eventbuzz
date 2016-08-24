32ff03313da8a43a9590b37d877f4c0739dd813c
joseph muli <jayjaymuli00@gmail.com>
2016-08-24 14:43:03 +0300

    (Feature) Create event issue
    add CRUD method that creates new event
    closes #1

--- operations.py
+++ operations.py
@@ -1,26 +1,38 @@
-from database import Event
-from sqlalchemy import sessionmaker
+import os
+
+from database import Event, engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 
 
 
 
 class EventsCrud(object):
 
-	engine = create_engine('sqlite://ebuzz.sqlite', echo = False)
-	
-	# a new query session
-	Session = sessionmaker(bind=engine)
+	engine = create_engine('sqlite:///ebuzz.sqlite', echo = False)
+
+	# create new session object for queries
+	Session = sessionmaker()
+	Session.configure(bind=engine)
 	session = Session()
 
+	def __init__(self, name, start_date, end_date, venue):
+		self.name = name
+		self.start_date = start_date
+		self.end_date = end_date
+		self.venue = venue
 
-	def create_event(self):
+	
+
+	def create_event(self, name, start_date, end_date, venue):
 		'''
-		creates a new event
+		creates a new event instance
 		'''
 		# creating a new event from Event class
 		event = Event(name, start_date, end_date, venue)
 
-		# add event object to session
+		# add event object to session for db commit
 		EventsCrud.session.add(event)
 		EventsCrud.session.commit()
 
@@ -41,3 +53,4 @@ class EventsCrud(object):
 		'''
 		events = EventsCrud.session.query.all()
 		return events
+
